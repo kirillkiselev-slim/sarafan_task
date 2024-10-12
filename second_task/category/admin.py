2edeb2328428d1085d07c4e09a529a54e9296f4b
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-10-13 01:40:35 +0300

    add admin category

--- second_task/category/admin.py
+++ second_task/category/admin.py
@@ -8,7 +8,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug',)
     search_fields = ('slug', 'title')
     list_filter = ('slug',)
-    list_display_links = ('slug',)
+    list_display_links = ('slug', 'title')
 
 
 class SubCategoryAdmin(admin.ModelAdmin):
@@ -17,15 +17,15 @@ class SubCategoryAdmin(admin.ModelAdmin):
     list_editable = ('category',)
     search_fields = ('slug', 'title')
     list_filter = ('slug',)
-    list_display_links = ('slug',)
+    list_display_links = ('slug', 'title')
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price',)
-    list_select_related = ('subcategory',)
-    list_editable = ('subcategory',)
-    search_fields = ('name',)
-    list_filter = ('subcategory',)
+    list_display = ('name', 'price', 'subcategory', 'category')
+    list_select_related = ('subcategory', 'category')
+    list_editable = ('subcategory', 'category')
+    list_filter = ('slug', 'subcategory', 'category')
+    search_fields = ('name', 'slug')
 
 
 admin.site.register(Category, CategoryAdmin)
