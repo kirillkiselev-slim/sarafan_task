b83636bccc28520241784e5f5b4bea2612ee5f0d
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-10-17 01:39:09 +0300

    delete unnecessary files

--- second_task/api/constants.py
+++ second_task/api/constants.py
@@ -6,3 +6,15 @@ NOT_IN_SHOPPING_CART = {
     'errors': 'Этого товара нет в корзине.'
 }
 SUCCESS_MESSAGE = {'success': 'Удалены товар/товары'}
+
+CATEGORY_TESTS = {'title': 'Электроника', 'slug': 'electronics'}
+SUBCATEGORY_TESTS = {'title': 'Электроника', 'slug': 'electronics'}
+
+START_TEST_SUBCATEGORY = 1
+END_TEST_SUBCATEGORY = 6
+
+POST_SHOPPING_CART = {'amount': 100}
+PUT_SHOPPING_CART = {'amount': 200}
+
+USER_CREDS = {'username': 'test_user'}
+
